984ff02c89575a6ed912ee71947576617177741a
kyle2023 <122161575+kyle2023@users.noreply.github.com>
2026-07-30 17:47:02 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -348,12 +348,14 @@ export default {
           "dlj.tf": "https://dlj.tf/short",
         },
         customBackend: {
+          "kyle提供-自用后端": "https://subapi.336555.xyz",
           "CM提供-负载均衡后端": "https://subapi.cmliussss.net",
           "CM提供-应急备用后端": "https://subapi.fxxk.dedyn.io",
           "肥羊提供-增强型后端": "https://url.v1.mk",
           "肥羊提供-备用后端": "https://api.v1.mk",
         },
         backendOptions: [
+          { value: "https://subapi.336555.xyz" },
           { value: "https://subapi.cmliussss.net" },
           { value: "https://subapi.fxxk.dedyn.io" },
           { value: "https://url.v1.mk" },
